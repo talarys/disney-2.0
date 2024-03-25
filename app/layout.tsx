c8c18e35719d0dce6592e40093baef8515ca2fe7
talarys <d.simon.ssx0@gmail.com>
2024-03-25 23:04:26 +0100

    Added intern as font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,51 +1,57 @@
-import type { Metadata } from 'next'
-import Header from '@/components/Header'
-import { ThemeProvider } from '@/components/theme-provider'
+import type { Metadata } from "next"
+import { Inter as FontSans } from "next/font/google"
+import Header from "@/components/Header"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils"
 
-import './globals.css'
+import "./globals.css"
+
+const fontSans = FontSans({
+	subsets: ["latin"],
+	variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
-    title: 'Disney+ 2.0',
-    description:
-        'Stream brand new Originals, blockbusters and binge-worthy series',
+	title: "Disney+ 2.0",
+	description:
+		"Stream brand new Originals, blockbusters and binge-worthy series",
 }
 
 export default function RootLayout({
-    children,
+	children,
 }: Readonly<{
-    children: React.ReactNode
+	children: React.ReactNode
 }>) {
-    return (
-        <html lang='en'>
-            <head>
-                <link rel='shortcut icon' href='/favicon/favicon.ico' />
-                <link
-                    rel='apple-touch-icon'
-                    sizes='180x180'
-                    href='/favicon/apple-touch-icon.png'
-                />
-                <link
-                    rel='icon'
-                    type='image/png'
-                    sizes='32x32'
-                    href='/favicon/favicon-32x32.png'
-                />
-                <link
-                    rel='icon'
-                    type='image/png'
-                    sizes='16x16'
-                    href='/favicon/favicon-16x16.png'
-                />
-            </head>
-            <body className='bg-white dark:bg-dbg'>
-                <ThemeProvider
-                    attribute='class'
-                    defaultTheme='system'
-                    enableSystem>
-                    <Header />
-                    {children}
-                </ThemeProvider>
-            </body>
-        </html>
-    )
+	return (
+		<html lang="en">
+			<head>
+				<link rel="shortcut icon" href="/favicon/favicon.ico" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/favicon/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon/favicon-16x16.png"
+				/>
+			</head>
+			<body
+				className={cn("dark:bg-dbg font-sans antialiased", fontSans.variable)}
+			>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+					<Header />
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	)
 }
