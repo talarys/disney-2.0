e6e9c2d7d7e0aa7c585fbafdd87ca9e960b0a68c
talarys <d.simon.ssx0@gmail.com>
2024-03-27 00:51:10 +0100

    Fixed width

--- components/MovieCardVertical.tsx
+++ components/MovieCardVertical.tsx
@@ -14,14 +14,12 @@ const MovieCardVertical = ({ movie }: { movie: Movie }) => {
 			}}
 			className="relative flex-shrink-0 cursor-pointer transform hover:scale-105 duration-200 ease-out hover:drop-shadow-lg z-30"
 		>
-			<div className="absolute inset-0 bg-gradient-to-b from-gray-200/0 via-gray-900/10 to-gray-300 dark:to-[#1a1c29]/80" />
-
 			<Image
 				alt={movie.title}
 				width={1080}
 				height={1920}
 				src={getImagePath(movie.poster_path || movie.backdrop_path)}
-				className="w-fit h-[300px] object-cover object-center shadow-md shadow-gray-900 drop-shadow-xl rounded-sm"
+				className="w-44 object-cover object-center shadow-md shadow-gray-900 drop-shadow-xl rounded-sm"
 			/>
 		</div>
 	)
