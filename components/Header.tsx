26075e146f8eddf48acc0b89f798420a16d7a6cd
talarys <d.simon.ssx0@gmail.com>
2024-03-26 12:43:12 +0100

    Fixed header Z

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ import GenreDropdown from "./GenreDropdown"
 
 const Header = () => {
 	return (
-		<header className="flex justify-between fixed w-full top-0 items-center p-5 bg-gradient-to-t from-gray-900/0 via-gray-900/25 to-gray-900">
+		<header className="flex justify-between fixed w-full top-0 items-center p-5 bg-gradient-to-t from-gray-900/0 via-gray-900/25 to-gray-900 z-50">
 			<Link href={"/"} className="mr-10">
 				<Image src={disneyLogo} alt="logo" className="w-32 cursor-pointer" />
 			</Link>
