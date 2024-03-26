d47a06bc5f49ad2d13a5ce8eb6bc04c65f0c8dd3
talarys <d.simon.ssx0@gmail.com>
2024-03-26 01:42:18 +0100

    Fix typo

--- lib/getImagePath.ts
+++ lib/getImagePath.ts
@@ -1,5 +1,7 @@
 export function getImagePath(imagePath?: string, fullSize?: boolean) {
 	return imagePath
-		? `https://image.tmdb.org/t/p/${fullSize ? "orignal" : "w500"}/${imagePath}`
+		? `https://image.tmdb.org/t/p/${
+				fullSize ? "original" : "w500"
+		  }/${imagePath}`
 		: "/image_fallback.png"
 }
