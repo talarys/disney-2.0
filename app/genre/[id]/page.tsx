62d73ec691877b1c58abe8e5f57f11a389da1a44
talarys <d.simon.ssx0@gmail.com>
2024-03-26 14:02:39 +0100

    Added genre page

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,22 +1,39 @@
-import { notFound } from 'next/navigation'
+import MoviesCarousel from "@/components/MoviesCarousel"
+import MoviesCarouselVertical from "@/components/MoviesCarouselVertical"
+import { getDiscoverMovies, getPopularMovies } from "@/lib/getMovies"
+import { notFound } from "next/navigation"
 
 type Props = {
-    params: {
-        id: string
-    }
-    searchParams: {
-        genre: string
-    }
+	params: {
+		id: string
+	}
+	searchParams: {
+		genre: string
+	}
 }
-const SearchPage = ({ params: { id }, searchParams: { genre } }: Props) => {
-    if (!id) notFound()
-    id = decodeURI(id)
-    genre = decodeURI(genre)
-    return (
-        <div>
-            {id} {genre}
-        </div>
-    )
+const SearchPage = async ({
+	params: { id },
+	searchParams: { genre },
+}: Props) => {
+	if (!id) notFound()
+	id = decodeURI(id)
+	genre = decodeURI(genre)
+
+	const movies = await getDiscoverMovies(id)
+	const popularMovies = await getPopularMovies()
+
+	return (
+		<div className="max-w-7xl mx-auto mt-32">
+			<div className="flex flex-col space-y-4">
+				<h1 className="text-6xl font-bold px-10">Results for {genre}</h1>
+				<MoviesCarouselVertical movies={movies} />
+			</div>
+			<div className="mt-10 mb-10">
+				<h1 className="text-6xl font-bold px-10">Popular Movies</h1>
+				<MoviesCarousel movies={popularMovies} />
+			</div>
+		</div>
+	)
 }
 
 export default SearchPage
