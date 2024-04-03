94dd2a19dc15e59c953d6dc9edf28cfa665cd617
talarys <d.simon.ssx0@gmail.com>
2024-04-03 17:42:37 +0200

    Added scroll

--- components/MoviesCarousel.tsx
+++ components/MoviesCarousel.tsx
@@ -1,26 +1,37 @@
-import type { Movie } from "@/typings"
-import MovieCardVertical from "./MovieCardVertical"
+'use client'
+import { useRef } from 'react'
+import { useDraggable } from 'react-use-draggable-scroll'
+import type { Movie } from '@/typings'
+
+import MovieCardVertical from './MovieCardVertical'
 
 type Props = {
-	title?: string
-	movies: Movie[]
+    title?: string
+    movies: Movie[]
 }
 const MoviesCarousel = ({ movies, title }: Props) => {
-	return (
-		<div className="z-30">
-			<h2 className="text-xl font-bold px-10 py-2">{title}</h2>
+    const ref =
+        useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>
 
-			<div
-				className={
-					"flex space-x-4  overflow-scroll scrollbar-hide px-5 lg:px-10 py-5"
-				}
-			>
-				{movies?.map(movie => (
-					<MovieCardVertical key={movie.id} movie={movie} />
-				))}
-			</div>
-		</div>
-	)
+    const { events } = useDraggable(ref, {
+        applyRubberBandEffect: true,
+    })
+    return (
+        <div className='z-30'>
+            <h2 className='text-xl font-bold px-10 py-2'>{title}</h2>
+
+            <div
+                {...events}
+                ref={ref}
+                className={
+                    'flex space-x-4  overflow-scroll scrollbar-hide px-5 lg:px-10 py-5'
+                }>
+                {movies?.map(movie => (
+                    <MovieCardVertical key={movie.id} movie={movie} />
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default MoviesCarousel
